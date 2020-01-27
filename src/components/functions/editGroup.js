4a3424c381e8c9770be93d9d2def0e5244b1cf32
Ethan Malin <ethanmalin01@gmail.com>
2020-01-26 21:24:41 -0500

    re-awaiting call

--- src/components/functions/editGroup.js
+++ src/components/functions/editGroup.js
@@ -1,67 +1,83 @@
 import firebase from 'react-native-firebase';
-import {unsubscribeUserFromTopics, sendNotificationToTopic} from '../functions/notifications';
+import {
+  unsubscribeUserFromTopics,
+  sendNotificationToTopic,
+} from '../functions/notifications';
 import {uploadPictureFirebase} from '../functions/pictures';
-
-
 
 // Should not be used to remove players. removeUserFromGroup(...) ensures the removed user will be notified of the event.
 async function editGroup(updatedGroup, callback = () => {}) {
-    // handle uploading of new image if one selected
-    if (updatedGroup.img) {
-        let uri = await uploadPictureFirebase(
-            uri,
-            'groups/' + updatedGroup.objectID,
-        );
-        delete updatedGroup.img;
-        updatedGroup = {
-            ...updatedGroup,
-            pictures: [uri],
-        };
-    }
-    await firebase
+  // handle uploading of new image if one selected
+  if (updatedGroup.img) {
+    let uri = await uploadPictureFirebase(
+      uri,
+      'groups/' + updatedGroup.objectID,
+    );
+    delete updatedGroup.img;
+    updatedGroup = {
+      ...updatedGroup,
+      pictures: [uri],
+    };
+  }
+  await firebase
     .database()
     .ref('groups/' + updatedGroup.objectID + '/')
     .update(updatedGroup)
     .then(() => callback)
-    .catch(err => {throw err;});
-    try {
-        var editNotif = {
-          notification: {
-            title: 'The group admin has edited the details of ' + updatedGroup.info.name,
-            body: '',
-            sound: 'default',
-          },
-          data: {
-            action: 'openGroupPage',
-            objectID: updatedGroup.objectID,
-          },
-        };
-        var topicGroup = '/topics/' + updatedGroup.objectID;
-        await sendNotificationToTopic(topicGroup, editNotif);
-      } catch (err) {
-        console.log(err.message);
-      }
-    return true;
+    .catch((err) => {
+      throw err;
+    });
+  try {
+    var editNotif = {
+      notification: {
+        title:
+          'The group admin has edited the details of ' + updatedGroup.info.name,
+        body: '',
+        sound: 'default',
+      },
+      data: {
+        action: 'openGroupPage',
+        objectID: updatedGroup.objectID,
+      },
+    };
+    var topicGroup = '/topics/' + updatedGroup.objectID;
+    await sendNotificationToTopic(topicGroup, editNotif);
+  } catch (err) {
+    console.log(err.message);
+  }
+  return true;
 }
 
 async function removeUserFromGroup(playerID, group) {
-    unsubscribeUserFromTopics(playerID, [group.objectID]);
-    if (group.allMembers) {
-        let index = group.allMembers.indexOf(playerID);
-        if (index !== -1) {
-            await firebase
-            .database()
-            .ref('groups/' + group.objectID + '/allMembers/' + index)
-            .remove()
-            .catch(err => {throw err;});
-        }
+  let res = await unsubscribeUserFromTopics(playerID, [group.objectID]);
+  if (!res) {
+    console.log(
+      'Could not unsubscribe user :' +
+        playerID +
+        ' from group: ' +
+        group.objectID,
+    );
+  }
+  if (group.allMembers) {
+    let index = group.allMembers.indexOf(playerID);
+    if (index !== -1) {
+      await firebase
+        .database()
+        .ref('groups/' + group.objectID + '/allMembers/' + index)
+        .remove()
+        .catch((err) => {
+          throw err;
+        });
     }
-    await firebase
+  }
+  await firebase
     .database()
     .ref('groups/' + group.objectID + '/members/' + playerID)
     .remove()
-    .catch(err => {throw err;});
-    return true;
+    .catch((err) => {
+      throw err;
+    });
+  return true;
 }
 
 module.exports = {editGroup, removeUserFromGroup};
