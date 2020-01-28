99ce273319da302d7a365df56e4c77803dea4314
Ethan Malin <ethanmalin01@gmail.com>
2020-01-28 17:50:17 -0500

    move main tab icon out of navigation

--- src/components/navigation/AppNavigator.js
+++ src/components/navigation/AppNavigator.js
@@ -9,8 +9,7 @@ import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 import colors from '../style/colors';
 import styles from '../style/style';
-import AllIcons from '../layout/icons/AllIcons';
-import Button from '../layout/Views/Button';
+import MainTabIcon from './navigationElements/MainTabIcon.js';
 
 import HomePage from '../app/HomePage';
 import StreamPage from '../app/StreamPage';
@@ -216,84 +215,12 @@ const MainApp = createBottomTabNavigator(
     defaultNavigationOptions: ({navigation}) => ({
       tabBarIcon: ({focused, tintColor}) => {
         const {routeName} = navigation.state;
-        var borderOff = 'white';
         return (
-          <Button
-            view={() => {
-              return (
-                <Row
-                  style={{
-                    height: '100%',
-                    borderTopWidth: 1.5,
-                    borderColor: focused ? colors.primary : 'transparent',
-                  }}>
-                  {routeName === 'MessageList' ? (
-                    <View style={styles.roundMessage} />
-                  ) : null}
-                  <Col size={10} style={[styles.center4, {paddingTop: 10}]}>
-                    <AllIcons
-                      name={
-                        routeName === 'Home'
-                          ? 'calendar2'
-                          : routeName === 'ListGroups'
-                          ? 'profileFooter'
-                          : routeName === 'Stream'
-                          ? 'camera'
-                          : routeName === 'MessageList'
-                          ? 'speech'
-                          : routeName === 'Profile'
-                          ? 'menu'
-                          : null
-                      }
-                      size={16}
-                      color={tintColor} // color red for stream button?
-                      style={styles.iconFooter}
-                      type={
-                        routeName === 'Stream'
-                          ? 'font'
-                          : 'moon'
-                      }
-                    />
-                    <Text
-                      style={[
-                        styles.footerText,
-                        {
-                          color: tintColor,
-                          marginTop: 6,
-                          marginBottom: 5,
-                          fontSize: 12.5,
-                        },
-                      ]}>
-                      {routeName === 'Home'
-                        ? 'Events'
-                        : routeName === 'ListGroups'
-                        ? 'Groups'
-                        : routeName === 'Stream'
-                        ? 'Go Live'
-                        : routeName === 'MessageList'
-                        ? 'Message'
-                        : routeName === 'Profile'
-                        ? 'Profile'
-                        : null}
-                    </Text>
-                  </Col>
-                </Row>
-              );
-            }}
-            click={() => navigation.navigate(routeName)}
-            color={'white'}
-            style={[
-              {
-                paddingTop: 0,
-                backgroundColor: 'white',
-                width: '100%',
-                height: '100%',
-                borderRadius: 0,
-                paddingLeft: 0,
-                paddingRight: 0,
-              },
-            ]}
-            onPressColor={colors.off2}
+          <MainTabIcon
+            navigation={navigation}
+            focused={focused}
+            tintColor={tintColor}s
+            routeName={routeName}
           />
         );
       },
