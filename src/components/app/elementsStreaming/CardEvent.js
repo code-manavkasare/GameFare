115895e7c473b34a39856ba1cad8a43919b954f9
Ethan Malin <ethanmalin01@gmail.com>
2020-02-03 21:06:47 -0500

    arrow on event card

--- src/components/app/elementsStreaming/CardEvent.js
+++ src/components/app/elementsStreaming/CardEvent.js
@@ -7,6 +7,7 @@ import {Col, Row} from 'react-native-easy-grid';
 import NavigationService from '../../../../NavigationService';
 import colors from '../../style/colors';
 import AsyncImage from '../../layout/image/AsyncImage';
+import AllIcons from '../../layout/icons/AllIcons';
 
 import ButtonColor from '../../layout/Views/Button';
 import styleApp from '../../style/style';
@@ -15,62 +16,6 @@ import {date, time} from '../../layout/date/date';
 class CardEvent extends React.Component {
   constructor(props) {
     super(props);
-  }
-  displayCard(data, attendees) {
-    var sport = Object.values(this.props.sports).filter(
-      (sport) => sport.value === data.info.sport,
-    )[0];
-    var league = Object.values(sport.typeEvent)
-      .filter((item) => item)
-      .filter((item) => item.value === data.info.league)[0];
-    if (!league) return null;
-
-    return (
-      <ButtonColor
-        key={this.props.index}
-        view={() => {
-          return (
-            <FadeInView duration={300} style={styleApp.fullSize}>
-              {this.props.league === 'all' && (
-                <AsyncImage
-                  style={styles.logoLeague}
-                  mainImage={league.icon}
-                  imgInitial={league.icon}
-                />
-              )}
-
-              <Text
-                style={[
-                  styleApp.input,
-                  {color: colors.primary2, fontSize: 12},
-                ]}>
-                {date(data.date.start, 'ddd, Do MMM')}{' '}
-                <Text style={{color: colors.title, fontSize: 10}}>•</Text>{' '}
-                {time(data.date.start, 'h:mm a')}
-              </Text>
-              <Text
-                style={[
-                  styleApp.input,
-                  {fontSize: 15, minHeight: 20, marginTop: 5},
-                ]}>
-                {data.info.name}
-              </Text>
-              <Text style={[styleApp.subtitle, {marginTop: 5, minHeight: 35}]}>
-                {data.location.address}
-              </Text>
-
-              {this.rowAttendees(attendees)}
-
-              {this.props.size === 'M' && <View style={styles.dividerBottom} />}
-            </FadeInView>
-          );
-        }}
-        click={() => NavigationService.push('Event', {objectID: data.objectID})}
-        color={'white'}
-        style={styleApp.cardEvent}
-        onPressColor={colors.off}
-      />
-    );
   }
 
   render() {
@@ -82,19 +27,32 @@ class CardEvent extends React.Component {
         onPressColor={colors.off}
         view={() => {
           return (
-            <Col style={styles.fullWidth}>
-              <Row style={styles.row}>
-                <Text style={styleApp.title}>{this.props.eventTitle}</Text>
-              </Row>
-              <Row style={styles.row}>
-                <Text
-                  style={[styleApp.input, {color: colors.primary2, fontSize: 12}]}>
-                  {date(this.props.start, 'ddd, Do MMM')}{' '}
-                  <Text style={{color: colors.title, fontSize: 12}}>•</Text>{' '}
-                  {time(this.props.start, 'h:mm a')}
-                </Text>
-              </Row>
-            </Col>
+            <Row style={styles.container}>
+              <Col size={90} style={styles.leftText}>
+                <Row style={styles.row}>
+                  <Text style={styleApp.title}>{this.props.eventTitle}</Text>
+                </Row>
+                <Row style={styles.row}>
+                  <Text
+                    style={[
+                      styleApp.input,
+                      {color: colors.primary2, fontSize: 12},
+                    ]}>
+                    {date(this.props.start, 'ddd, Do MMM')}{' '}
+                    <Text style={{color: colors.title, fontSize: 12}}>•</Text>{' '}
+                    {time(this.props.start, 'h:mm a')}
+                  </Text>
+                </Row>
+              </Col>
+              <Col size={10} style={styleApp.center2}>
+                <AllIcons
+                  type="mat"
+                  size={15}
+                  color={colors.black}
+                  name="keyboard-arrow-right"
+                />
+              </Col>
+            </Row>
           );
         }}
       />
@@ -113,10 +71,18 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     borderColor: colors.borderColor,
   },
+  container: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   row: {
     width: '100%',
     justifyContent: 'flex-start',
     alignItems: 'center',
+  },
+  leftText: {
+    alignItems: 'flex-start',
   },
   fullWidth: {
     width: '100%',
