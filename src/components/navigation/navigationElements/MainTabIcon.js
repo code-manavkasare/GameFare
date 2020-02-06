0d70c8044d24add968c946ac3fd8a693906e32f8
birolleauf@gmail.com <birolleauf@gmail.com>
2020-02-06 13:10:35 +1100

    polish footer button component

--- src/components/navigation/navigationElements/MainTabIcon.js
+++ src/components/navigation/navigationElements/MainTabIcon.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, styleAppheet, StyleSheet} from 'react-native';
 import {Col, Row} from 'react-native-easy-grid';
 import {connect} from 'react-redux';
 
 import colors from '../../style/colors';
-import styles from '../../style/style';
+import styleApp from '../../style/style';
 import AllIcons from '../../layout/icons/AllIcons';
 import Button from '../../layout/Views/Button';
 
@@ -16,91 +16,87 @@ class MainTabIcon extends React.Component {
   render() {
     const {navigation, focused, tintColor, routeName} = this.props;
     return (
-        <Button
-          view={() => {
-            return (
-              <Row
-                style={{
-                  height: '100%',
-                  borderTopWidth: 1.5,
-                  borderColor: focused ? colors.primary : 'transparent',
-                }}>
-                {routeName === 'MessageList' ? (
-                  <View style={styles.roundMessage} />
-                ) : null}
-                <Col size={10} style={[styles.center4, {paddingTop: 10}]}>
-                  <AllIcons
-                    name={
-                      routeName === 'Home'
-                        ? 'calendar2'
-                        : routeName === 'ListGroups'
-                        ? 'profileFooter'
-                        : routeName === 'Stream'
-                        ? 'camera'
-                        : routeName === 'MessageList'
-                        ? 'speech'
-                        : routeName === 'Profile'
-                        ? 'menu'
-                        : null
-                    }
-                    size={16}
-                    color={tintColor} // color red for stream button?
-                    style={styles.iconFooter}
-                    type={
-                      routeName === 'Stream'
-                        ? 'font'
-                        : 'moon'
-                    }
-                  />
-                  <Text
-                    style={[
-                      styles.footerText,
-                      {
-                        color: tintColor,
-                        marginTop: 6,
-                        marginBottom: 5,
-                        fontSize: 12.5,
-                      },
-                    ]}>
-                    {routeName === 'Home'
-                      ? 'Events'
+      <Button
+        view={() => {
+          return (
+            <Row
+              style={[
+                styles.rowInButton,
+                {borderColor: focused ? colors.primary : 'transparent'},
+              ]}>
+              {routeName === 'MessageList' ? (
+                <View style={styleApp.roundMessage} />
+              ) : null}
+              <Col size={10} style={[styleApp.center4, {paddingTop: 10}]}>
+                <AllIcons
+                  name={
+                    routeName === 'Home'
+                      ? 'calendar2'
                       : routeName === 'ListGroups'
-                      ? 'Groups'
+                      ? 'profileFooter'
                       : routeName === 'Stream'
-                      ? 'Go Live'
+                      ? 'video-camera'
                       : routeName === 'MessageList'
-                      ? 'Message'
+                      ? 'speech'
                       : routeName === 'Profile'
-                      ? 'Profile'
-                      : null}
-                  </Text>
-                </Col>
-              </Row>
-            );
-          }}
-          click={() => navigation.navigate(routeName)}
-          color={'white'}
-          style={[
-            {
-              paddingTop: 0,
-              backgroundColor: 'white',
-              width: '100%',
-              height: '100%',
-              borderRadius: 0,
-              paddingLeft: 0,
-              paddingRight: 0,
-            },
-          ]}
-          onPressColor={colors.off2}
-        />
-      );
+                      ? 'menu'
+                      : null
+                  }
+                  size={16}
+                  color={tintColor} // color red for stream button?
+                  style={styleApp.iconFooter}
+                  type={'moon'}
+                />
+                <Text style={[styles.textButton, {color: tintColor}]}>
+                  {routeName === 'Home'
+                    ? 'Events'
+                    : routeName === 'ListGroups'
+                    ? 'Groups'
+                    : routeName === 'Stream'
+                    ? 'Go Live'
+                    : routeName === 'MessageList'
+                    ? 'Message'
+                    : routeName === 'Profile'
+                    ? 'Profile'
+                    : null}
+                </Text>
+              </Col>
+            </Row>
+          );
+        }}
+        click={() => navigation.navigate(routeName)}
+        color={'white'}
+        style={styles.button}
+        onPressColor={colors.off2}
+      />
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    paddingTop: 0,
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    borderRadius: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  textButton: {
+    ...styleApp.footerText,
+    marginTop: 6,
+    marginBottom: 5,
+    fontSize: 12.5,
+  },
+  rowInButton: {
+    height: '100%',
+    borderTopWidth: 1.5,
+  },
+});
 
 const mapStateToProps = (state) => {
   return {};
 };
 
 export default connect(mapStateToProps, {})(MainTabIcon);
-
-
