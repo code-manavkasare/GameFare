99cd3e43aeb7c12ea4b365aa974a2f3a5213d145
Ethan Malin <ethanmalin01@gmail.com>
2020-02-14 19:42:11 -0500

    secured streaming functions

--- src/components/functions/streaming.js
+++ src/components/functions/streaming.js
@@ -1,10 +1,11 @@
 import {uploadPictureFirebase} from '../functions/pictures';
 import firebase from 'react-native-firebase';
 import axios from 'axios';
+import Config from 'react-native-config';
 
-const MUX_TOKEN_ID = 'cbc3b201-74d4-42ce-9296-a516a1c0d11d';
-const MUX_TOKEN_SECRET =
-  'pH0xdGK3b7qCA/kH8PSNspLqyLa+BJnsjnY4OBtHzECpDg6efuho2RdFsRgKkDqutbCkzAHS9Q1';
+// const MUX_TOKEN_ID = 'cbc3b201-74d4-42ce-9296-a516a1c0d11d';
+// const MUX_TOKEN_SECRET =
+//   'pH0xdGK3b7qCA/kH8PSNspLqyLa+BJnsjnY4OBtHzECpDg6efuho2RdFsRgKkDqutbCkzAHS9Q1';
 
 async function createStreamFirebase(stream, eventID) {
   const firebaseStream = {
@@ -17,9 +18,9 @@ async function createStreamFirebase(stream, eventID) {
     .set(firebaseStream);
 }
 
-async function createStream(eventID) {
-  var url = 'https://api.mux.com/video/v1/live-streams';
-  const response = await axios.post(
+async function createStreamMux() {
+  const url = Config.MUX_LIVE_STREAM_URL;
+  axios.post(
     url,
     {
       playback_policy: ['public'],
@@ -29,35 +30,84 @@ async function createStream(eventID) {
     },
     {
       auth: {
-        username: MUX_TOKEN_ID,
-        password: MUX_TOKEN_SECRET,
+        username: `${Config.MUX_TOKEN_ID}`,
+        password: `${Config.MUX_TOKEN_SECRET}`,
       },
     },
-  );
-  const stream = {
-    streamKey: response.data.data.stream_key,
-    playbackID: response.data.data.playback_ids[0].id,
-    id: response.data.data.id,
-  };
-  await createStreamFirebase(stream, eventID);
+  )
+  .then((response) => {
+    const stream = {
+      streamKey: response.data.data.stream_key,
+      playbackID: response.data.data.playback_ids[0].id,
+      id: response.data.data.id,
+    };
+    return stream;
+  })
+  .catch((error) => {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('ERROR: createStreamMux: ', error.message);
+    }
+    console.log(error.config);
+    return false;
+  });
+
+
+}
+
+async function createStream(eventID) {
+  const stream = await createStreamMux();
+  if (stream) {
+    await createStreamFirebase(stream, eventID);
+  }
   return stream;
 }
 
-async function destroyStream(streamID, error) {
-  var url = 'https://api.mux.com/video/v1/live-streams/' + streamID;
-  await axios.delete(url, {
-    auth: {
-      username: MUX_TOKEN_ID,
-      password: MUX_TOKEN_SECRET,
-    },
-  });
-  if (!error) {
-    await firebase
+async function destroyStreamFirebase(streamID) {
+  await firebase
     .database()
     .ref('streams/' + streamID + '/')
-    .remove();
-  }
+    .remove()
+    .catch(function(error) {
+      console.log(
+        'ERROR: destroyStreamFirebase: ' + error.message,
+      );
+    });
+}
 
+async function destroyStreamMux(streamID) {
+  const url = `${Config.MUX_LIVE_STREAM_URL}streamID`;
+  await axios
+    .delete(url, {
+      auth: {
+        username: `${Config.MUX_TOKEN_ID}`,
+        password: `${Config.MUX_TOKEN_SECRET}`,
+      },
+    })
+    .catch(function(error) {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('ERROR: destroyStreamMux: ', error.message);
+      }
+      console.log(error.config);
+    });
+}
+
+async function destroyStream(streamID, saveFirebase) {
+  await destroyStreamMux(streamID);
+  if (!saveFirebase) {
+    destroyStreamFirebase(streamID);
+  }
 }
 
 async function uploadNetlinePhoto(streamID, uri) {
