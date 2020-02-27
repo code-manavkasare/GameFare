2cbc266a9b316728d1b1870fd62d1513dbf8a1f4
Raphael Abadie <rafa.abadie@gmail.com>
2020-02-27 18:05:22 +0000

    Delete appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Update Bundle Version with package.json Version
-
-echo VERSION SET IS :
-node -pe "require('./package.json').version"
-echo $PACKAGE_VERSION
-plutil -replace CFBundleVersion -string $PACKAGE_VERSION $APPCENTER_SOURCE_DIRECTORY/ios/gamefare/Info.plist
-plutil -replace CFBundleVersion -string $PACKAGE_VERSION $APPCENTER_SOURCE_DIRECTORY/ios/gamefare-dev-Info.plist
