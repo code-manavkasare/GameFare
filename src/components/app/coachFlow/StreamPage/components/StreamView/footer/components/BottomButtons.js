2882f24e49ae4405f397e270440413cdcc88141d
Ethan Malin <ethanmalin01@gmail.com>
2020-07-20 12:49:44 -0500

    rem conslogs

--- src/components/app/coachFlow/StreamPage/components/StreamView/footer/components/BottomButtons.js
+++ src/components/app/coachFlow/StreamPage/components/StreamView/footer/components/BottomButtons.js
@@ -133,7 +133,6 @@ class BottomButton extends Component {
     queue.addWorker(new Worker('stopRecording', this.stopRecording.bind(this)));
   }
   startRemoteRecording = async (member) => {
-    console.log('startRemoteRecording');
     const {coachSessionID, userID} = this.props;
     const recordingUser = member.id;
     await startRemoteRecording(recordingUser, coachSessionID, userID);
