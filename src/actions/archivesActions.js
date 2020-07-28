393a8016fa71532b49a37b677c10b96df7502886
Raphael Abadie <rafa.abadie@gmail.com>
2020-07-28 17:08:43 +0000

    fix remove video and session crash

--- src/actions/archivesActions.js
+++ src/actions/archivesActions.js
@@ -1,8 +1,21 @@
-import {SET_ARCHIVE} from './types';
+import database from '@react-native-firebase/database';
+
+import {DELETE_ARCHIVE, RESET_ARCHIVES, SET_ARCHIVE} from './types';
 
 const setArchive = (value) => ({
   type: SET_ARCHIVE,
   archive: value,
 });
 
-export {setArchive};
+const resetArchives = () => ({
+  type: RESET_ARCHIVES,
+});
+
+const deleteArchive = (archiveId) => {
+  database()
+    .ref(`archivedStreams/${archiveId}`)
+    .off();
+  return {type: DELETE_ARCHIVE, archiveId};
+};
+
+export {deleteArchive, resetArchives, setArchive};
