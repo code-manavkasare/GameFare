9b8a9ffb1f849b219918801a74f6cf1e5ccc39c1
birolleauf@gmail.com <birolleauf@gmail.com>
2020-10-19 19:32:51 +1100

    polish

--- src/components/app/videoPlayerPage/components/drawing/shapes/DrawAngles.js
+++ src/components/app/videoPlayerPage/components/drawing/shapes/DrawAngles.js
@@ -27,20 +27,6 @@ class DrawSraightLine extends Component {
     const alpha = Math.atan2(cross, dot);
 
     return Math.abs(Math.floor((alpha * 180) / Math.PI + 0.5));
-    // const angba = Math.atan((point1.y - point2.y) / (point1.x - point2.x));
-    // const angbc = Math.atan((point3.y - point2.y) / (point3.x - point2.y));
-    // const rslt = angba - angbc;
-    // const rs = (rslt * 180) / 3.141592;
-    // return rs;
-    // const a = point2.x - point1.x;
-    // const b = point2.y - point1.y;
-    // const c = point2.x - point3.x;
-    // const d = point2.y - point3.y;
-
-    // const atanA = Math.atan2(a, b);
-    // const atanB = Math.atan2(c, d);
-
-    // return (atanB - atanA) * 180;
   }
   drawView() {
     const {
@@ -66,9 +52,6 @@ class DrawSraightLine extends Component {
 
     const x1c = (3 * x1 + x3) / 4;
     const y1c = (3 * y1 + y3) / 4;
-
-    const xAngle = x1 + (x12 - x1) * Math.cos(180) - (y12 - y1) * Math.sin(180);
-    const yAngle = y1 + (y12 - y1) * Math.cos(180) + (x12 - x1) * Math.sin(180);
 
     return (
       <G>
@@ -122,7 +105,7 @@ class DrawSraightLine extends Component {
           stroke={strokeColor}
           strokeWidth={strokeWidth}
         />
- 
+
         <EditPoint
           startPoint={startPoint}
           id={id}
