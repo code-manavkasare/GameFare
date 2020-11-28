890839a0b975925c098c2d7d3163ef93775def01
Pumposh Bhat <pumposh@live.com>
2020-11-27 21:17:22 -0600

    use rn image to manage image update logic

--- src/components/layout/image/AsyncImage.js
+++ src/components/layout/image/AsyncImage.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import {object, oneOfType, string, array} from 'prop-types';
 import {View, Animated, Image} from 'react-native';
-import FastImage from 'react-native-fast-image';
 
 import {native} from '../../animations/animations';
 import {timeout} from '../../functions/coach';
-import {sentryCaptureException} from '../../functions/logs.js';
 
 export default class AsyncImage extends Component {
   static propTypes = {
@@ -24,25 +22,18 @@ export default class AsyncImage extends Component {
       zIndexCached: 10,
       checkToken: '',
     };
-    this.opacityFastImageCached = new Animated.Value(1);
-  }
-  componentDidMount() {
-    // this.preloadImage();
+    this.opacityFastImageCached = new Animated.Value(0);
   }
   componentDidUpdate(prevProps, prevState) {
-    // if (this.props.mainImage !== prevProps.mainImage) {
-    //   this.preloadImage();
-    // }
+    if (this.props.mainImage !== prevProps.mainImage) {
+      this.preloadImage();
+    }
   }
   async preloadImage() {
-    await FastImage.preload([
-      {
-        uri: this.props.mainImage,
-        cache: FastImage.cacheControl.web,
-      },
-    ]);
+    const {mainImage} = this.props;
+    await Image.prefetch(mainImage);
     await timeout(1000);
-    this.setState({imagePath: this.props.mainImage});
+    this.setState({imagePath: mainImage});
   }
   enterPictureCached() {
     Animated.timing(this.opacityFastImageCached, native(1)).start();
@@ -55,7 +46,7 @@ export default class AsyncImage extends Component {
     return mainImage;
   }
   imgDisplay() {
-    const {style, resizeMode, onError} = this.props;
+    const {style} = this.props;
     const {imagePath} = this.state;
     return (
       <Animated.View
@@ -65,30 +56,12 @@ export default class AsyncImage extends Component {
           width: style.width,
         }}>
         <Image
+          onLoadEnd={() => {
+            this.enterPictureCached();
+          }}
           source={{uri: imagePath}}
           style={[style, {zIndex: 10, position: 'absolute', top: 0}]}
         />
-        {/* <FastImage
-          resizeMode={resizeMode ? resizeMode : 'cover'}
-          onLoadEnd={(callback) => {
-            console.log('callback', callback);
-            // this.enterPictureCached();
-          }}
-          style={[style, {zIndex: 10, position: 'absolute', top: 0}]}
-          source={{
-            cache: FastImage.cacheControl.web,
-            uri: imagePath,
-          }}
-          onError={(response) => {
-            if (onError) onError();
-            sentryCaptureException({
-              event: 'FastImageError',
-              error: response,
-              props: this.props,
-              state: this.state,
-            });
-          }}
-        /> */}
       </Animated.View>
     );
   }
