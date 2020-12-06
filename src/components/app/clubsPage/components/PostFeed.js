4e6896f7ee45613877aa7c145c690352b7e8d2bb
Pumposh Bhat <pumposh@live.com>
2020-12-05 20:34:28 -0600

    move club options row to separate component

--- src/components/app/clubsPage/components/PostFeed.js
+++ src/components/app/clubsPage/components/PostFeed.js
@@ -1,27 +1,21 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import {any, string} from 'prop-types';
-import {Row} from 'react-native-easy-grid';
 
-import styleApp from '../../../style/style';
+import {FlatListComponent} from '../../../layout/Views/FlatList';
+import CardPost from './CardPost';
+import OptionsRow from './OptionsRow';
 import {
   postListSelector,
   isClubOwnerSelector,
 } from '../../../../store/selectors/clubs';
-import {FlatListComponent} from '../../../layout/Views/FlatList';
-import CardPost from './CardPost';
-import colors from '../../../style/colors';
+import styleApp from '../../../style/style';
 import {
   heightFooter,
   heightHeaderHome,
   marginBottomApp,
   marginTopApp,
 } from '../../../style/sizes';
-import {navigate} from '../../../../../NavigationService';
-import ButtonColor from '../../../layout/Views/Button';
-import AllIcons from '../../../layout/icons/AllIcons';
-import {inviteUsersToClub} from '../../../functions/clubs';
 
 class PostFeed extends Component {
   static propTypes = {
@@ -43,81 +37,13 @@ class PostFeed extends Component {
       this.flatListRef?.jumpToTop();
     }
   }
-  createPost = () => {
-    const {currentClubID} = this.props;
-    navigate('CreatePost', {clubID: currentClubID});
-  };
-  goToSettings = () => {
-    const {currentClubID} = this.props;
-    navigate('Club', {screen: 'ClubSettings', params: {id: currentClubID}});
-  };
-  inviteUsersToClub = () => {
-    const {currentClubID} = this.props;
-    inviteUsersToClub({clubID: currentClubID});
-  };
   renderClub = ({item: {id: postID}}) => {
     const {currentClubID} = this.props;
     return <CardPost id={postID} clubID={currentClubID} />;
   };
   settingsRow = () => {
-    const {isClubOwner} = this.props;
-    if (!isClubOwner) return null;
-    return (
-      <View style={styles.settingsRowContainer}>
-        <Row style={styles.settingsRow}>
-          <ButtonColor
-            view={() => {
-              return (
-                <AllIcons
-                  type={'font'}
-                  color={colors.white}
-                  size={16}
-                  name={'user-plus'}
-                  solid
-                />
-              );
-            }}
-            style={styles.settingsRowButton}
-            color={colors.greyDark}
-            click={this.inviteUsersToClub}
-            onPressColor={colors.greyMidDark}
-          />
-          <ButtonColor
-            view={() => {
-              return (
-                <AllIcons
-                  type={'font'}
-                  color={colors.white}
-                  size={16}
-                  name={'cog'}
-                />
-              );
-            }}
-            style={styles.settingsRowButton}
-            color={colors.greyDark}
-            click={this.goToSettings}
-            onPressColor={colors.greyMidDark}
-          />
-          <ButtonColor
-            view={() => {
-              return (
-                <AllIcons
-                  type={'font'}
-                  color={colors.white}
-                  size={16}
-                  name={'plus'}
-                  solid
-                />
-              );
-            }}
-            style={styles.settingsRowButton}
-            color={colors.blue}
-            click={this.createPost}
-            onPressColor={colors.blueLight}
-          />
-        </Row>
-      </View>
-    );
+    const {currentClubID} = this.props;
+    return <OptionsRow currentClubID={currentClubID} />;
   };
   emptyList = () => {
     const {isClubOwner} = this.props;
@@ -161,37 +87,6 @@ class PostFeed extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  postButton: {
-    width: '100%',
-    borderBottomWidth: 0,
-    borderColor: colors.off,
-    borderRadius: 15,
-    height: 40,
-    marginBottom: 30,
-    shadowOpacity: 0,
-  },
-  settingsRowContainer: {
-    height: 70,
-    width: '100%',
-    ...styleApp.center2,
-  },
-  settingsRow: {
-    width: '50%',
-    maxWidth: 240,
-    minHeight: 55,
-    ...styleApp.center,
-    justifyContent: 'space-between',
-  },
-  settingsRowButton: {
-    width: 45,
-    height: 45,
-    borderRadius: 25,
-    marginTop: -10,
-    ...styleApp.shadowWeak,
-  },
-});
 
 const mapStateToProps = (state, props) => {
   return {
